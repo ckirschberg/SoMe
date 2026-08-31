94b7f40a839d6d8ee26abf8c6f5a57bedd9c55cf
Christian Kirschberg <ckirschberg@gmail.com>
2026-08-31 10:40:50 +0200

    .

--- src/post/dtos/create-post.dto.ts
+++ src/post/dtos/create-post.dto.ts
@@ -1,5 +1,15 @@
+import { IsNotEmpty, IsString } from 'class-validator';
+
 export class CreatePostDto {
+    @IsString()
+    @IsNotEmpty()
     title: string;
+
+    @IsString()
+    @IsNotEmpty()
     body: string;
+
+    @IsString()
+    @IsNotEmpty()
     author: string;
 }

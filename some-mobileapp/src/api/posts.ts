e375e5266edf32bd31175d52c55325347bc1f984
Christian Kirschberg <ckirschberg@gmail.com>
2026-09-12 15:58:30 +0200

    .

--- some-mobileapp/src/api/posts.ts
+++ some-mobileapp/src/api/posts.ts
@@ -1,14 +1,27 @@
 import { Platform } from "react-native"
+import { NewPostDto } from "../types/newPostDto";
 
 const baseUrl = Platform.OS === 'ios' ? 'localhost:3006' : '10.0.2.2:3006';
 
-export default async function fetchPosts() {
+export async function fetchPosts() {
     var result = await fetch(baseUrl + '/posts');
 
     if (!result.ok) {
-        
+        throw new Error("Error fetching posts");
     }
 
     return result.json();
 }
 
+export async function addPost(dto: NewPostDto) {
+    var result = await fetch(baseUrl + "/posts", {
+        method: "POST",
+        body: JSON.stringify(dto)
+    });
+
+    if (!result.ok) {
+        throw new Error("Error saving post");
+    }
+
+    return result.json();
+}
---
+++ some-mobileapp/src/types/newPostDto.ts
@@ -0,0 +1,3 @@
+export class NewPostDto {
+    constructor(public title: string, public body: string, public author: string) {}
+}

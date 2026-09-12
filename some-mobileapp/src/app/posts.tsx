eac50123fb3a7401c9e1d15c040028512b477529
Christian Kirschberg <ckirschberg@gmail.com>
2026-09-12 16:21:28 +0200

    .

--- some-mobileapp/src/app/posts.tsx
+++ some-mobileapp/src/app/posts.tsx
@@ -1,14 +1,24 @@
 import { useEffect, useState } from "react";
 import { View } from "react-native";
 import { fetchPosts } from "../api/posts";
+import { PostDto } from "../types/postDto";
 
 export default function Posts() {
-    const [posts, setPosts] = useState([])
+    const [posts, setPosts] = useState<PostDto[]>([])
+    const [error, setError] = useState<string>("")
 
-    
+    const loadPosts = async () => {
+        try {
+            const posts = await fetchPosts()
+            setPosts(posts);
+        } catch (e) {
+            setError(e.message)
+        }
+
+    }
     
     useEffect(() => {
-        fetchPosts()
+        
     }, [])
 
 

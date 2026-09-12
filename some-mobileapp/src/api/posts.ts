bdd5b5ddf49058622a687fc401f7f4fd5fc46a63
Christian Kirschberg <ckirschberg@gmail.com>
2026-09-12 15:49:50 +0200

    .

--- some-mobileapp/src/api/posts.ts
+++ some-mobileapp/src/api/posts.ts
@@ -5,7 +5,9 @@ const baseUrl = Platform.OS === 'ios' ? 'localhost:3006' : '10.0.2.2:3006';
 export default async function fetchPosts() {
     var result = await fetch(baseUrl + '/posts');
 
-    
+    if (!result.ok) {
+        
+    }
 
     return result.json();
 }
